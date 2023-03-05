6b259f776c1e7caf5a163949f7ae6da4aefd16cd
debanjanhanu <115066308+debanjanhanu@users.noreply.github.com>
2023-03-06 02:34:27 +0530

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -1,16 +1,16 @@
 echo $first_val
 echo $second_val
 echo $operator_val
-if [$operator_val=="+"]; then
+if [$operator_val=="+ "]; then
   res=$(expr $first_val + $second_val)
   echo $res
-elif [$operator_val=="-"]; then
+elif [$operator_val=="- "]; then
   res=$(expr $first_val - $second_val)
   echo $res
-elif [$operator_val=="*"]; then
+elif [$operator_val=="* "]; then
   res=$(expr $first_val * $second_val)
   echo $res
-elif [$operator_val=="/"]; then
+elif [$operator_val=="/ "]; then
   res=$(expr $first_val * $second_val)
   echo $res
 else

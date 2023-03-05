e5726c325b566ac73f116c41b70cff274ef538c6
debanjanhanu <115066308+debanjanhanu@users.noreply.github.com>
2023-03-06 02:26:13 +0530

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -1,16 +1,6 @@
-if [$operator_val=='+']; then
-  res=$(expr $first_val + $second_val)
-  echo $res
-elif [$operator_val=='-']; then
-  res=$(expr $first_val - $second_val)
-  echo $res
-elif [$operator_val=='*']; then
-  res=$(expr $first_val * $second_val)
-  echo $res
-else
-  res=$(expr $first_val / $second_val)
-  echo $res
-fi
+echo $first_val
+echo $second_val
+echo $operator_val
 
 
 

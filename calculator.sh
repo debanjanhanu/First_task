84765332261a79d0ac922ef5aed3e8081950a675
debanjanhanu <115066308+debanjanhanu@users.noreply.github.com>
2023-03-06 08:20:33 +0530

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -1,6 +1,6 @@
 if [ "$operator_val" = "+" ]; then
   res=$(expr $first_val + $second_val)
-  echo -e "Addition result is" ; echo $res
+  echo -e "Addition result is $res"
 elif [ "$operator_val" = "-" ]; then
   res=$(expr $first_val - $second_val)
   echo $res

f5e4578b7102824dfed1a85e2025120cfe706508
debanjanhanu <115066308+debanjanhanu@users.noreply.github.com>
2023-03-06 08:39:36 +0530

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -5,13 +5,8 @@ elif [ "$operator_val" = "-" ]; then
   res=$(expr $first_val - $second_val)
   echo "Substraction result is $res"
 elif [ "$operator_val" = "*" ]; then
-  for i in {1..$second_val}
-  do
-    res = $res + $first_val
-  res=$(expr $first_val * $second_val)
-  done
-  #res = $($4 * $3)
-  echo "Multiplication result is $res"
+  resu = $(expr $first_val * $second_val)
+  echo "Multiplication result is $resu"
 elif [ "$operator_val" = "/" ]; then
   res=$(expr $first_val / $second_val)
   echo "Division result is $res"
